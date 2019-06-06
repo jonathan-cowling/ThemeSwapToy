37811060bae832ef3b7ea5afd90afe604ca95b8a
Jonathan Cowling <jonathan.cowling@moneysupermarket.com>
2019-06-06 16:21:50 +0100

    Add transparent status bar

--- app/src/main/java/com/moneysupermarket/cowling/jonathan/themeswaptoy/TestFragment.kt
+++ app/src/main/java/com/moneysupermarket/cowling/jonathan/themeswaptoy/TestFragment.kt
@@ -1,15 +1,19 @@
 package com.moneysupermarket.cowling.jonathan.themeswaptoy
 
 
+import android.graphics.Color
 import android.os.Bundle
 import android.preference.PreferenceManager
-import android.view.ContextThemeWrapper
+import android.view.*
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_test.*
 
 class TestFragment : Fragment() {
+
+    private var oldWindowFlags: Int = 0
+    private var oldStatusBarColor: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,10 +29,44 @@ class TestFragment : Fragment() {
             else -> R.style.AppTheme_Red
         }
 
+        requireActivity().window.apply {
+            oldStatusBarColor = statusBarColor
+            oldWindowFlags = attributes.flags
+
+
+            statusBarColor = Color.TRANSPARENT
+            addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+            clearFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(this,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    teardown()
+                    findNavController().popBackStack()
+                }
+            }
+        )
+
         return inflater
             .cloneInContext(ContextThemeWrapper(requireContext(), theme))
             .inflate(R.layout.fragment_test, container, false)
     }
 
+    private fun teardown() {
+        requireActivity().window.apply {
+            statusBarColor = oldStatusBarColor
+            setFlags(oldWindowFlags, -1)
+        }
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button.setOnClickListener {
+            teardown()
+            findNavController().navigate(R.id.action_testFragment_to_test2)
+        }
+    }
 }
